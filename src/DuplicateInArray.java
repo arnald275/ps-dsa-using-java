87e519d5512b656e496477457111f69d4ea105d9
arnald <arnald275@gmail.com>
2024-11-25 21:49:48 +0530

    using sort to find duplicates in array

--- src/DuplicateInArray.java
+++ src/DuplicateInArray.java
@@ -1,7 +1,10 @@
 package src;
 
+import java.util.Arrays;
+
 public class DuplicateInArray {
 
+    // O(n^2)
     private static void bruteForce(int[] a){
         for(int i=0; i<a.length; i++){
             for(int j=i+1; j<a.length; j++){
@@ -10,7 +13,16 @@ public class DuplicateInArray {
         }
     }
 
+
+    public static void usingSort(int[] a){
+        Arrays.sort(a);
+        for(int i=0; i<a.length-1 ; i++){
+            if(a[i] == a[i+1]) System.out.println("duplicate found : " + a[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        bruteForce(new int[]{1, 1, 3, 4, 5, 6, 6, 9, 3});
+//        bruteForce(new int[]{1, 1, 3, 4, 5, 6, 6, 9, 3});
+        usingSort(new int[]{1, 1, 3, 4, 5, 6, 6, 9, 3});
     }
 }
